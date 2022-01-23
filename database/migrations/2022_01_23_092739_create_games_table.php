488a406782725541e5575a93f1404b142bc3319d
Matthieu MARTIN <codingstuff91@gmail.com>
2022-01-23 11:22:17 +0100

    Model and db migration

---
+++ database/migrations/2022_01_23_092739_create_games_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('number_of_players');
+            $table->integer('minimum_age');
+            $table->integer('game_duration_minimum');
+            $table->integer('game_duration_maximum');
+            $table->integer('release_year');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
