f3b6f1b4a872dfe39de6b3bfea7e82e0b46749b1
Matthieu MARTIN <codingstuff91@gmail.com>
2022-01-23 11:43:00 +0100

    Initiation des routes d'API

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GameController;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| Public API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
 */
+Route::prefix('games')->group(function(){
+    route::get('/', [GameController::class, 'index']);
+});
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+/*
+|--------------------------------------------------------------------------
+| Protected API Routes
+|--------------------------------------------------------------------------
+*/
+Route::prefix('games')->middleware(['auth:sanctum'])->group(function(){
 });
